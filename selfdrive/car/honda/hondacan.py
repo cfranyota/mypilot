050243e637f1e8709f7a31ba43aec3f9216642ee
BogGyver <39277124+BogGyver@users.noreply.github.com>
2019-04-26 23:07:20 -0400

    Update hondacan.py

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -200,12 +200,12 @@ def spam_buttons_command(packer, button_val, idx):
   }
   return packer.make_can_msg("SCM_BUTTONS", 0, values, idx)
 
-def create_radar_VIN_msg(id,radarVIN,radarCAN,radarTriggerMessage,useRadar):
+def create_radar_VIN_msg(id,radarVIN,radarCAN,radarTriggerMessage,useRadar,radarPosition,radarEpasType):
   msg_id = 0x560
   msg_len = 8
   msg = create_string_buffer(msg_len)
   if id == 0:
-    struct.pack_into('BBBBBBBB', msg, 0, id,radarCAN,useRadar,((radarTriggerMessage >> 8) & 0xFF),(radarTriggerMessage & 0xFF),ord(radarVIN[0]),ord(radarVIN[1]),ord(radarVIN[2]))
+    struct.pack_into('BBBBBBBB', msg, 0, id,radarCAN,useRadar + (radarPosition << 1) + (radarEpasType << 3),((radarTriggerMessage >> 8) & 0xFF),(radarTriggerMessage & 0xFF),ord(radarVIN[0]),ord(radarVIN[1]),ord(radarVIN[2]))
   if id == 1:
     struct.pack_into('BBBBBBBB', msg, 0, id,ord(radarVIN[3]),ord(radarVIN[4]),ord(radarVIN[5]),ord(radarVIN[6]),ord(radarVIN[7]),ord(radarVIN[8]),ord(radarVIN[9]))
   if id == 2:
