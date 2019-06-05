2ed2d02cbf1b420d620ebcc86e1d1c8f97043347
Vaggysag <48258491+Vaggysag@users.noreply.github.com>
2019-06-05 12:26:17 -0400

    Tweaked cruise_min

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -24,13 +24,13 @@ AWARENESS_DECEL = -0.2     # car smoothly decel at .2m/s^2 when user is distract
 
 # lookup tables VS speed to determine min and max accels in cruise
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MIN_V  = [-3.5, -3.3, -2.0, -1., -.69]
+_A_CRUISE_MIN_V  = [-5.0, -4.5, -3.0, -1., -.69]
 _A_CRUISE_MIN_BP = [   0., 5.,  10., 20.,  40.]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
 _A_CRUISE_MAX_V = [4.8, 4.5, 3.65, 1.15, 1.1]
-_A_CRUISE_MAX_V_FOLLOWING = [5.3, 5.1, 3.8, 1.2, 1.1]
+_A_CRUISE_MAX_V_FOLLOWING = [5.3, 5.1, 3.8, 1.1, 1.0]
 _A_CRUISE_MAX_BP = [0., 5., 10., 20., 40.]
 
 # Lookup table for turns
