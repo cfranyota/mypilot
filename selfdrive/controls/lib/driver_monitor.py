681397d39df808fb22dc89df7413fc25b2a9d679
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2020-02-22 09:25:13 -0800

    2x the distracted values - test #1

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -6,11 +6,11 @@ from common.filter_simple import FirstOrderFilter
 from common.stat_live import RunningStatFilter
 
 _AWARENESS_TIME = 180.  # Three minutes limit without user touching steering wheels make the car enter a terminal status
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 15.  # a first alert is issued 25s before expiration
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.  # a second alert is issued 15s before start decelerating the car
-_DISTRACTED_TIME = 11.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 30.  # a first alert is issued 25s before expiration
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 12.  # a second alert is issued 15s before start decelerating the car
+_DISTRACTED_TIME = 22.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 16.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 12.
 
 _FACE_THRESHOLD = 0.4
 _EYE_THRESHOLD = 0.6
