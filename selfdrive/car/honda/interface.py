dffdc7a475bfa90b909059e8cb1a53b5b0eeee92
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2020-02-26 12:56:23 -0800

    Revert to stock kf test

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -178,7 +178,7 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 1.
 
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.1], [0.03]] if eps_modified else [[0.8], [0.24]]
-      ret.lateralTuning.pid.kf = 0.00007 #Testing 4x OG torque curve mod
+      ret.lateralTuning.pid.kf = 0.00006 #Testing 4x OG torque curve mod
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
       ret.longitudinalTuning.kiBP = [0., 35.]
