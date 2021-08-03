9acdce3e31018d471f466fd1efe63cfdbce476af
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-08-03 06:52:15 -0400

    Custom Speed Offset

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -600,6 +600,10 @@ class Controls:
     controlsState.angleSteers = CS.steeringAngleDeg
     controlsState.steeringAngleDesiredDeg = actuators.steeringAngleDeg
 
+    # personal speedometer offset
+    if self.v_cruise_kph != 255:
+      controlsState.vCruise = controlsState.vCruise * 1.0076
+
     if self.joystick_mode:
       controlsState.lateralControlState.debugState = lac_log
     elif self.CP.steerControlType == car.CarParams.SteerControlType.angle:
